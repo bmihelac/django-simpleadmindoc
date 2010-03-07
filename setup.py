cfce137fe23f010b3fdf2c4deb987a344f839c9b
Bojan Mihelac <bmihelac@lijenovo.(none)>
2010-03-07 20:08:49 +0100

    update packages in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "Django Simple admin doc",
     version = "0.1",
     author = "Bojan Mihelac",
     author_email = "bmihelac@mihelac.org",
-    packages = find_packages(),
+    packages = ['simpleadmindoc'],
 )
