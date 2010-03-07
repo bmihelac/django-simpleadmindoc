7653ee60dce217ed19ef9573b02d035212720eb9
Bojan Mihelac <bmihelac@lijenovo.(none)>
2010-03-07 18:26:51 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Django Simple admin doc",
+    version = "0.1",
+    author = "Bojan Mihelac",
+    author_email = "bmihelac@mihelac.org",
+    packages = find_packages(),
+)
