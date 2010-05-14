7f711f7da0003cf6f0335f33fb358e91d4e91cf7
Bojan Mihelac <bmihelac@mihelac.org>
2010-05-14 11:38:08 +0200

    Add option to set locale for created documentation

--- simpleadmindoc/management/commands/docgenapp.py
+++ simpleadmindoc/management/commands/docgenapp.py
@@ -1,11 +1,22 @@
+from optparse import make_option
+
 from django.core.management.base import AppCommand
 
 
 class Command(AppCommand):
     help = "Generate sphinx documentation skeleton for given apps."
+    option_list = AppCommand.option_list + (
+            make_option('--locale', '-l', default=None, dest='locale',
+                help='Use given locale'),
+        )    
     
     def handle_app(self, app, **options):
         # check if simpleadmindoc directory is setup
+        locale = options.get('locale', None)
+        if locale:
+            from django.utils import translation
+            translation.activate(locale)
+            
         from django.db import models
         from simpleadmindoc.generate import generate_model_doc, generate_app_doc, generate_index_doc
         generate_index_doc()
