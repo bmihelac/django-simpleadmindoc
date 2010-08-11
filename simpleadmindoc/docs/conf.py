cbc98bbf3b5fa62f7132cde4205e2553d079808e
Bojan Mihelac <bmihelac@mihelac.org>
2010-08-11 10:48:29 +0200

    add docs configuration file

---
+++ simpleadmindoc/docs/conf.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+master_doc = 'index'
+
+project = u'django-simpleadmindoc'
+copyright = u'2010, Bojan Mihelac'
+version = '0'
+release = '0.3'
+
+# language = 'en'
+html_theme = 'nature'
+
+rst_epilog = """
+.. |project_name| replace:: django-simpleadmindoc, InformatikaMihelac
+"""
